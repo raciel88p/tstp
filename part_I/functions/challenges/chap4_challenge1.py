6b10d61aa02f36514bfa7dc2b603929f987d9901
raciel88p <roweb88@gmail.com>
2020-04-15 14:05:08 -0600

    Update for better work

--- part_I/functions/challenges/chap4_challenge1.py
+++ part_I/functions/challenges/chap4_challenge1.py
@@ -2,3 +2,12 @@ def squared(x):
     return x ** 2
 
 print(squared(2))
+
+
+
+These the way that work for me:
+x = input ("ingresa un numero")
+x = int(x)
+def alcuaadrado (x):
+    return x**2
+print (alcuaadrado(x))
