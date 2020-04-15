16fcce9d6729caa93b49a9c2a70d85d396746859
raciel88p <roweb88@gmail.com>
2020-04-15 16:30:39 -0600

    update

--- part_I/functions/challenges/chap4_challenge3.py
+++ part_I/functions/challenges/chap4_challenge3.py
@@ -3,3 +3,19 @@ def add_mult(a,b,c,x=100,z=1000):
 
 result = add_mult(10, 15, 25)
 print(result)
+
+a = input ("Ingrese un numero")
+a = int (a)
+d = input ("Ingrese otro numero")
+d = int (d)
+f = input ("ingrese otro valor numerico")
+f = int (f)
+x = 50
+z = 36
+
+def prueba(a,d,f,x,z):
+    return a + d + f - x -z
+resultado = prueba(a, d, f, x, z)
+print(resultado)
+
+
