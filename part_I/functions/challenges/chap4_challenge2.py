3dc34e5f08b0005182c4972cf9920ea044ce755b
raciel88p <roweb88@gmail.com>
2020-04-15 14:35:04 -0600

    update

--- part_I/functions/challenges/chap4_challenge2.py
+++ part_I/functions/challenges/chap4_challenge2.py
@@ -2,3 +2,13 @@ def print_string(string):
     print(string)
 
 print_string("Testing: 1, 2, 3.")
+
+
+
+
+
+a = input ("ingresa un texto")
+a = str (a)
+def detodo(a):
+    return a
+print (detodo(a))
