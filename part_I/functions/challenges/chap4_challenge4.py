8f08dae7ee794198e65df026087814d7e8f713c7
raciel88p <roweb88@gmail.com>
2020-04-15 18:41:16 -0600

    Update Roberto

--- part_I/functions/challenges/chap4_challenge4.py
+++ part_I/functions/challenges/chap4_challenge4.py
@@ -9,3 +9,18 @@ y = divide(4)
 z = multiply(y)
 
 print(z)
+
+
+
+x = input ("Ingresa un numero")
+x = int(x)
+def a (x):
+    return x / 2
+a(x)
+print(a(x))
+#otra funcion
+z = x
+z = int(z)
+def s (z):
+    return z * 4
+print(s(z))
